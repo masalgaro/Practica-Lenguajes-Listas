7f1099e25f28830c12f34b0e68d449d5a32e3bef
masalgaro <124186064+masalgaro@users.noreply.github.com>
2023-09-19 09:14:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 int main() {
-    srand(time(NULL));
+    srand(time(NULL)); //Mide segundos del reloj del sistema desde el 1 de enero de 1970
     int headData = rand() % 100 + 1;
-    int headCient = rand() % 2 + 1; //Para asegurar que el head también genere un evento valido
+    int headCient = rand() % 2 + 1; //Para asegurar que el head también genere un evento válido
     Lista *l = new Lista();
     l->crearLista();
     l->insertarCabezaLista(headData, headCient);
